e04f048315e569f5ec6fbc2709df5cce4e5e8c44
Alberto Hernández <30424171+albertobikee@users.noreply.github.com>
2025-02-10 09:06:25 +0100

    Rama 1

--- src/SegundaParteExamen/Examen2.java
+++ src/SegundaParteExamen/Examen2.java
@@ -1,5 +1,41 @@
 package SegundaParteExamen;
 
-public class Examen2 {
+import java.util.Scanner;
 
+public class Examen2 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner leer = new Scanner(System.in);
+		
+		System.out.println("Examen de geografía. IES FLORENCIO PINTADO");
+		System.out.println("1. ¿Cuál es el país más grande del mundo por superficie?");
+		String respuesta = leer.nextLine();
+		
+		if (respuesta.equalsIgnoreCase("Rusia")) 
+		{
+			System.out.println("¡¡CORRECTO!!");
+		}else {
+			System.out.println("ERROR...");
+		}
+		
+		System.out.println("2. ¿En qué continente se encuentra Egipto?");
+		String respuesta2 = leer.nextLine();
+		
+		if (respuesta2.equalsIgnoreCase("África")) 
+		{
+			System.out.println("¡¡CORRECTO!!");
+		}else {
+			System.out.println("ERROR...");
+		}
+		
+		System.out.println("3. ¿Cuál es el río más largo del mundo?");
+		String respuesta3 = leer.nextLine();
+		
+		if (respuesta3.equalsIgnoreCase("Amazonas")) 
+		{
+			System.out.println("¡¡CORRECTO!!");
+		}else {
+			System.out.println("ERROR...");
+		}
+	}
 }
