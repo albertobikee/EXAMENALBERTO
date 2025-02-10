7cbca443aae3f47b27b376a4eb334241c1e6bc50
Alberto Hernández <30424171+albertobikee@users.noreply.github.com>
2025-02-10 08:54:22 +0100

    Clase Examen

---
+++ src/Examen/Examen.java
@@ -0,0 +1,14 @@
+package Examen;
+
+import java.util.Scanner;
+
+public class Examen {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner leer = new Scanner(System.in);
+		
+		System.out.println("Examen de geografía. IES FLORENCIO PINTADO");
+	}
+
+}
