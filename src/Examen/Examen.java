f13fa08436ada9e1223fbf0cbf4f610dec254406
Alberto Hernández <30424171+albertobikee@users.noreply.github.com>
2025-02-10 08:57:00 +0100

    Versión1

--- src/Examen/Examen.java
+++ src/Examen/Examen.java
@@ -9,6 +9,15 @@ public class Examen {
 		Scanner leer = new Scanner(System.in);
 		
 		System.out.println("Examen de geografía. IES FLORENCIO PINTADO");
+		System.out.println("1. ¿Cuál es la capital de España?");
+		String respuesta = leer.nextLine();
+		
+		if (respuesta.equals("Madrid")) 
+		{
+			System.out.println("¡¡CORRECTO!!");
+		}else {
+			System.out.println("ERROR...");
+		}
 	}
 
 }
